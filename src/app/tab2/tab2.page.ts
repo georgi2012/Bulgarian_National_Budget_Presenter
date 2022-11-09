88a3d3324d72bff123d54dfda01c7bf12d08856d
Georgi Harlev <georgi.harlev@kongsbergdigital.com>
2022-11-09 17:44:08 +0200

    Added swipe transition to other tabs

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,37 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+//import { ItemService } from '../../services/item.service';
+import { NavController } from '@ionic/angular';
+//import { Item } from '../../models/item.model';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {
+  item: any;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    // private itemService: ItemService,
+    private navCtrl: NavController) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  onSwipeLeft($event) {
+    this.router.navigate(['/tabs/tab1']);
+    console.log("left swipe");
+  }
+
+  onSwipeRight($event) {
+    this.router.navigate(['/tabs/tab3']);
+    console.log("right swipe");
   }
 
 }
